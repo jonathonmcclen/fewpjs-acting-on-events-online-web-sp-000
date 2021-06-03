713556aea736f09495cc121bac35126e517bc531
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-06-03 19:32:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ function moveDodgerLeft() {
 }
 
 
-function moveDodgerRight(){
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
 }
